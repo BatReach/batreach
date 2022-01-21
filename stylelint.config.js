8742ab600ee4eb45eebff1b7af8c7d486a23d68b
Amer <amerk86@gmail.com>
2022-01-21 14:53:59 +1000

    stylelint, auto format on save fixed. finally!

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,20 +1,59 @@
+// See discussion at https://github.com/nuxt/create-nuxt-app/issues/446
+// Source https://github.com/ebrahimMaher/dalilmobarmg/blob/448b469432c4764d4fe886a671e3305b79a52c74/stylelint.config.js
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  "extends": [
+    'stylelint-config-recommended-scss',
+    "stylelint-config-prettier",
+    "stylelint-config-html",
+  ],
+
+  "plugins": [
+    "stylelint-scss", 
+    "stylelint-prettier"
+  ],
+
   // add your custom config here
-  // https://stylelint.io/user-guide/configuration
+  // Rule lists:
+  // - https://stylelint.io/user-guide/rules/
+  // - https://github.com/kristerkari/stylelint-scss#list-of-rules
   rules: {
+    "prettier/prettier": true,
+
+    // Tailwind
     'at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: [
-          'extend',
           'tailwind',
-          'screen',
           'apply',
-          'include',
+          'responsive',
+          'media',
+          'screen',
           'mixin',
-        ],
-      },
+          'include',
+          'extend'
+        ]
+      }
     ],
-  },
-}
+
+    // ===
+    // SCSS
+    // ===
+    // Allow SCSS keywords beginning with `@` for Tailwind
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'media',
+          'screen',
+          'mixin',
+          'include'
+        ]
+      }
+    ]
+  }
+};
